99461c0839816dfa675e7aee36b8e7a514e016bb
Mauro Werder <mauro3@runbox.com>
2020-09-25 18:36:10 +0200

    tidy and fix example.jl

--- examples/example.jl
+++ examples/example.jl
@@ -21,12 +21,11 @@ xs = -1.5:dx:1
 ys = -0.5:dx:3.0
 dem = ele.(xs, ys', randfac=0.1, withpit=true);
 plotyes && WWF.heatmap(xs,ys,dem)
-area, slen, dir, nout, nin, pits  = WWF.waterflows(dem, drain_pits=true);
+area, slen, dir, nout, nin, pits, c, bnds  = WWF.waterflows(dem, drain_pits=true);
 @assert size(dem)==(length(xs), length(ys))
 plotyes && WWF.plotit(xs,ys,dem)
 plotyes && WWF.plotarea(xs, ys, area, pits)
 
-c, bnds = WWF.catchments(dir, pits);
 plotyes && WWF.heatmap(xs, ys, c)
 
 demf = WWF.fill_dem(dem, pits, dir) #, small=1e-6)
