1576972d917e95d964168c48f19500d576694b10
Mauro Werder <mauro3@runbox.com>
2026-05-19 13:50:54 +0200

    Documenter execution

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,19 @@
-using Documenter, WhereTheWaterFlows
+using Documenter, WhereTheWaterFlows, CairoMakie
 
 makedocs(;
-    modules=[WhereTheWaterFlows],
-    format=Documenter.HTML(),
-    pages=[
+    modules = [WhereTheWaterFlows],
+    format  = Documenter.HTML(),
+    pages   = [
         "Home"     => "index.md",
         "Tutorial" => "tutorial.md",
         "API"      => "api.md",
     ],
-    repo="https://github.com/mauro3/WhereTheWaterFlows.jl/blob/{commit}{path}#L{line}",
-    sitename="WhereTheWaterFlows.jl",
-    authors="Mauro Werder <mauro3@runbox.com>",
+    repo     = "https://github.com/mauro3/WhereTheWaterFlows.jl/blob/{commit}{path}#L{line}",
+    sitename = "WhereTheWaterFlows.jl",
+    authors  = "Mauro Werder <mauro3@runbox.com>",
 )
 
 deploydocs(;
-    repo="github.com/mauro3/WhereTheWaterFlows.jl.git",
+    repo         = "github.com/mauro3/WhereTheWaterFlows.jl.git",
     push_preview = true,
 )
